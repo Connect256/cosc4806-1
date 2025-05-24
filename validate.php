53a7b4cdf1b7ce341756e57045d70f0e639bcf48
Connect256 <ruqia.h.farah@gmail.com>
2025-05-24 03:24:03 +0000

    created session variable

--- validate.php
+++ validate.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $valid_username = "ruqia";
 $valid_password = "password";
 
@@ -9,6 +10,12 @@ if($valid_username == $username && $valid_password == $password){
   echo "success"; 
 }
 else{
-  echo "failed";
+  if(!isset($_SESSION['failed_attempts'])){
+    $_SESSION['failed_attempts']= 1;
+  }
+  else{
+    $_SESSION['failed attempts'] = $_SESSION['failed_attempts'] + 1;
+  }
+  echo "This is unsuccessful attempt number " .$_SESSION['failed_attempts'];
 }
 ?>
