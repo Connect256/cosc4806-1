4b99d7a4c3beabf5e9eaceb63b1455ebecad9950
Connect256 <ruqia.h.farah@gmail.com>
2025-05-24 03:54:22 +0000

    created a logout page

--- index.php
+++ index.php
@@ -10,4 +10,8 @@ session_start();
     <h1> Assignment 1 </h1>
     <p>Welcome, <?= $_SESSION['username']?></p>
     <p><a href="/login.php">Click here to login</a></p>
+  </body>
+  <footer>
+    <p><a href="/logout.php"> Click here to logout </a></p>
+  </footer>
 </html>
